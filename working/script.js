a58c884d60a9d5bbbbe833fe36a1ba00303d69a1
Alek Novkovski <alek@greatcode2.com>
2023-05-12 22:05:12 +0200

    Finish program

--- working/script.js
+++ working/script.js
@@ -1,5 +1,14 @@
 const part1 = ["Why did the chicken cross the road?", "Why did it rain today?", "Why is life unfair?"]
-const part2 = ["2+2 is 4", "Red + Yellow Makes Orange", "You can't divide by zero"]
-const part3 = ["Some things don't make sense", "Not everything is fair", "Randomness is a part of the universe"]
+const part2 = ["Well...", "Because...", "The answer is simply that..."]
+const part3 = ["Some things don't make sense.", "Not everything is fair.", "Randomness is a part of the universe."]
 
-console.log("just testing")
+let message = ""
+function randomItem() {
+    return Math.floor(Math.random() * 3)
+}
+
+message += part1[randomItem()]
+message += " " + part2[randomItem()]
+message += " " + part3[randomItem()]
+
+console.log(message)

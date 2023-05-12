23b157b877d2440d2ce5d3b64d8b77e731310dee
Alek Novkovski <alek@greatcode2.com>
2023-05-12 21:50:47 +0200

    Add messages data

--- working/script.js
+++ working/script.js
@@ -1,1 +1,5 @@
+const part1 = ["Why did the chicken cross the road?", "Why did it rain today?", "Why is life unfair?"]
+const part2 = ["2+2 is 4", "Red + Yellow Makes Orange", "You can't divide by zero"]
+const part3 = ["Some things don't make sense", "Not everything is fair", "Randomness is a part of the universe"]
+
 console.log("just testing")
